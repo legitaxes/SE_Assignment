0b665a6f0c8a91951a8852af131e13e5d5136173
Lee Thin Tsin, John <leethintsinjohn@gmail.com>
2021-02-06 19:17:41 +0800

    Lecturer Iterator
    Followed the class diagram roughly to implement the following:
    IStudentIterator
    IStudentList
    StudentIterator
    StudentList
    classes
    
    Currently, inputting a staff ID on login will iterate through the list of lecturers. If it matches, you will be logged in and welcomed with your lecturer name
    
    Functionality of Lecuter Iterator will remain basic until further progress in project is made and changes are needed.
    
    Changed some spacing in StudentList.cs for clarity
    
    Changed Lecturer.cs to a public class and changed spacing
    
    Made changes in program.cs to add lecturers to a LecturerList lecturerCollection.
    
    Then made changes in the login code to use the iterator to check if there is a match in the lecturerCollection.
    
    Also changed names of the studentiterator instance in program.cs for clarity

---
+++ LecturerIterator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEAssignment
+{
+    public class LecturerIterator : ILecturerIterator
+    {
+        private LecturerList lecturerList;
+        private int position = 0;
+        private string staffID;
+        public bool isfound = false;
+
+        public LecturerIterator(LecturerList list, string staffID)
+        {
+            lecturerList = list;
+            this.staffID = staffID;
+
+            while (position < lecturerList.NumberOfLecturers)
+            {
+                if (lecturerList.LecturerCollection[position].StaffID == staffID)
+                {
+                    isfound = true;
+                    break;
+                }
+
+                else
+                {
+                    position++;
+                }
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (position < lecturerList.NumberOfLecturers || isfound == false)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public object Found()
+        {
+            Lecturer lecturer = lecturerList.LecturerCollection[position];
+            return lecturer;
+        }
+
+        public void Remove()
+        {
+            Console.WriteLine("Remove not supported by Lecturer Iterator");
+        }
+    }
+}
